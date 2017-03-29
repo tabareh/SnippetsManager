87f8f7a98ded1809403766f592b266737f989ea4
Abdollah Tabareh <abdollah.tabareh@hiq.se>
2017-03-29 13:07:56 +0200

    adding routing to app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,8 @@ import { Tag } from './models';
 import { Observable } from 'rxjs';
 import { ConfigService } from './shared';
 
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -12,55 +14,6 @@ import { ConfigService } from './shared';
 export class AppComponent {
 
 
-  private tags: Tag[] = [];
-  private searchInput: string = "";
-
-  private get filteredTags(): Tag[] {
-    return this.tags.filter(t => t.name.includes(this.searchInput));
-
-  }
-
-  constructor(private http: Http,
-    private config: ConfigService) {
-    this.fetchTags();
-  }
-
-  public fetchTags(): void {
-    this.tags = [];
-    this.http.get(`${this.config.baseApiUrl}/api/tags`).subscribe(res => {
-      console.log(res);
-      const body: any = res.json();
-      for (const item of body) {
-        const tag: Tag = <Tag>item;
-        this.tags.push(tag);
-      }
-      console.log(this.tags);
-    });
-  }
-
-  public addTag() {
-    this.http.post(`${this.config.baseApiUrl}/api/tags`, { name: this.searchInput }).subscribe(res => {
-      console.log(res);
-      this.fetchTags();
-    });
-  }
-
-  public removeTag(id: number) {
-    this.http.delete(`${this.config.baseApiUrl}/api/tags/${id}`).subscribe(res => {
-      console.log(res);
-      this.fetchTags();
-    });
-  }
-
-  public updateTag(id: number) {
-    const tag: Tag = this.tags.filter(t => t.id === id)[0];
-    // console.log('updating ' + JSON.stringify(tag));
-    // console.log('all tags after:' + JSON.stringify(this.tags));
-    this.http.put(`${this.config.baseApiUrl}/api/tags/${id}`, tag).subscribe(res => {
-      console.log(res);
-      this.fetchTags();
-    });
-  }
 
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,20 +4,29 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { SharedModule } from './shared';
 import { AppComponent } from './app.component';
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { TagsComponent } from './tags/tags.component';
+import { RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+
+export const ROUTES = [
+  { path: '', component: HomeComponent },
+  { path: 'tags', component: TagsComponent }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    TagsComponent
+    TagsComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
     SharedModule,
-    NgbModule.forRoot()
+    NgbModule.forRoot(),
+    RouterModule.forRoot(ROUTES)
   ],
   providers: [],
   bootstrap: [AppComponent]
